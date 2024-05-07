dbf595c36c158624013f78cc8d8b9166d46a5908
Jakub Bak <245762@edu.p.lodz.pl>
2024-05-07 16:58:20 +0200

    Starting screen - without image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,16 +9,51 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Widget buildContainer(String text, BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(15),
+      width: screenWidth * 0.55,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(49, 62, 80, 1.0),
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontFamily: 'Asap',
+          fontSize: 18,
+          letterSpacing: 0.5,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.blue,
-            title: Text("data", textAlign: TextAlign.center)
-        ),
+        backgroundColor: Color.fromRGBO(71, 82, 98, 1.0),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              buildContainer('LOGOWANIE', context),
+              SizedBox(height: screenHeight * 0.05),
+              buildContainer('REJESTRACJA', context)
+            ],
+          ),
+        )
       ),
     );
   }
