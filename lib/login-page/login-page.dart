0c99b4043943feebefb315dfb3e6fd21d6afb096
Jakub Bak <245762@edu.p.lodz.pl>
2024-05-08 22:57:32 +0200

    Creating Login Page

---
+++ lib/login-page/login-page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ekran Logowania'),
+      ),
+      body: Center(
+        child: Text('To jest ekran logowania'),
+      ),
+    );
+  }
+}
