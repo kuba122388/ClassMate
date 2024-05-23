fce57e4cc5e913a72965bcb901ac6e9ba16dca67
Ola <245818@edu.p.lodz.pl>
2024-05-23 21:19:22 +0200

    Added Announcements Page

---
+++ lib/user-page/announcements-page.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../consts/consts.dart';
+
+class AnnouncementsPage extends StatefulWidget {
+  const AnnouncementsPage({super.key});
+
+  @override
+  State<AnnouncementsPage> createState() => _AnnouncementsPageState();
+}
+
+class _AnnouncementsPageState extends State<AnnouncementsPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: COLOR_BACKGROUND,
+        appBar: BuildTopNav(context),
+        body: BuildBody(context),
+        bottomNavigationBar: BuildBackButton(context));
+  }
+
+  Center BuildBody(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Center(
+
+    );
+  }
+
+
+
+  PreferredSizeWidget BuildTopNav(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(kToolbarHeight),
+      child: Container(
+        padding: const EdgeInsets.only(top: 10, bottom: 10),
+        decoration: const BoxDecoration(
+            color: COLOR_BACKGROUND_DARKER,
+            borderRadius: BorderRadius.vertical(
+              bottom: BORDER_RADIUS,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(0, 2.0),
+                blurRadius: 4.0,
+              )
+            ]),
+        child: Center(
+          child: Text(
+            'Ogłoszenia',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Asap',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  BottomAppBar BuildBackButton(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return BottomAppBar(
+      color: Colors.transparent,
+      padding: EdgeInsets.zero,
+      child: Container(
+        decoration: const BoxDecoration(
+            color: COLOR_BACKGROUND_DARKER,
+            borderRadius: BorderRadius.vertical(top: BORDER_RADIUS)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Image.asset('././images/back_icon.png',
+                  height: screenHeight * 0.06),
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
+}
