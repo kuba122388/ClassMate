6ce1609b5977fd886db300e1d15545846e5c3901
Jakub <245762@edu.p.lodz.pl>
2024-05-23 02:19:53 +0200

    Added Register-approved screen Register page adjustments: Replaced InputField with Date widget while choosing date of birth Added dropdown list in university input field Changed form of saving data in Firebase - now e-mail and password is stored in Authentication tab and all information about user is stored in Firestore Database Added Delay and changed time of Snackbar

--- lib/register-page/register-page.dart
+++ lib/register-page/register-page.dart
@@ -1,7 +1,9 @@
+import 'package:classmate/register-page/register-approved.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../consts/consts.dart';
+import '../database-features/DatabaseFeatures.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -15,14 +17,19 @@ class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
-  final  _imieController = TextEditingController();
-  final  _nazwiskoController = TextEditingController();
-  final  _emailController = TextEditingController();
-  final  _dataUrodzeniaController = TextEditingController();
-  final  _uczelniaController = TextEditingController();
-  final  _hasloController = TextEditingController();
+  final _imieController = TextEditingController();
+  final _nazwiskoController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _dataUrodzeniaController = TextEditingController();
+  final _uczelniaController = TextEditingController();
+  final _hasloController = TextEditingController();
+  late DateTime _selectedDate;
 
-  late DatabaseReference dbRef;
+  final List<String> Uczelnie = [
+    'Politechnika Łódzka',
+    'Politechnika Wrocławska',
+    'Politechnika Częstochowska'
+  ];
 
   Map<String, bool> fieldErrors = {
     'Imię': false,
@@ -33,9 +40,25 @@ class _RegisterPageState extends State<RegisterPage> {
     'Hasło': false,
   };
 
-  @override initState(){
-    super.initState();
-    dbRef = FirebaseDatabase.instance.ref().child('Users');
+  void _presentDatePicker() {
+    DateTime now = DateTime.now();
+    DateTime lastDate = DateTime(now.year - 16, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: lastDate,
+      firstDate: DateTime(1900),
+      lastDate: lastDate,
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+        _dataUrodzeniaController.text =
+            DateFormat('dd-MM-yyyy').format(_selectedDate);
+        fieldErrors['Data urodzenia'] = true;
+      });
+    });
   }
 
   @override
@@ -87,7 +110,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   InputWithTitle(context, 'Nazwisko', _nazwiskoController),
                   InputWithTitle(context, 'E-mail', _emailController),
                   InputWithTitle(context, 'Data urodzenia', _dataUrodzeniaController),
-                  InputWithTitle(context, 'Uczelnia', _uczelniaController),
+                  DropDownList(context, 'Uczelnia', _uczelniaController),
                   InputWithTitle(context, 'Hasło', _hasloController),
                   Container(
                     margin: const EdgeInsets.only(top: 30, bottom: 40),
@@ -95,44 +118,53 @@ class _RegisterPageState extends State<RegisterPage> {
                       onPressed: () async {
                         if (_formKey.currentState!.validate()) {
                           try {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text("Przetwarzanie danych"),
-                                ));
-                            UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content: Text("Przetwarzanie danych"),
+                              duration: Duration(seconds: 1),
+                            ));
+
+                            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                                    email: _emailController.text,
+                                    password: _hasloController.text);
+
+                            List<String> dateParts = _dataUrodzeniaController.text.split('-');
+                            DateTime dateOfBirth = DateTime(int.parse(dateParts[2]), int.parse(dateParts[1]), int.parse(dateParts[0]));
+
+                            DatabaseUser User = DatabaseUser(
+                                firstName: _imieController.text,
+                                lastName: _nazwiskoController.text,
                                 email: _emailController.text,
-                                password: _hasloController.text
+                                dateOfBirth: dateOfBirth,
+                                university: _uczelniaController.text);
+
+                            saveUserData(User);
+
+                            await Future.delayed(
+                                const Duration(milliseconds: 1200));
+
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const RegisterApproved()),
                             );
-
-                            User? user = userCredential.user;
-
-                            Map<String, String> students = {
-                              'Imię': _imieController.text,
-                              'Nazwisko': _nazwiskoController.text,
-                              'E-mail': _emailController.text,
-                              'Data urodzenia': _dataUrodzeniaController.text,
-                              'Uczelnia': _uczelniaController.text,
-                            };
-                            //FirebaseFirestore.instance.collection('users').doc
-                            dbRef.push().set(students);
-
                           } on FirebaseAuthException catch (e) {
                             if (e.code == 'weak-password') {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text("Wprowadzone hasło jest za słabe"),
-                                  ));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content:
+                                    Text("Wprowadzone hasło jest za słabe"),
+                              ));
                             } else if (e.code == 'email-already-in-use') {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text("Ten adres E-mail już istnieje"),
-                                  ));
-                            }
-                            else{
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content: Text("Ten adres E-mail już istnieje"),
+                              ));
+                            } else {
                               print('UNKNOWN ERROR: ${e.code}');
                             }
                           }
-
                         }
                       },
                       style: ElevatedButton.styleFrom(
@@ -185,7 +217,8 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
-  Widget InputWithTitle(BuildContext context, String title, TextEditingController controller) {
+  Widget InputWithTitle(
+      BuildContext context, String title, TextEditingController controller) {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Column(
@@ -210,6 +243,12 @@ class _RegisterPageState extends State<RegisterPage> {
             borderRadius: BorderRadius.all(BORDER_RADIUS_INPUTS),
           ),
           child: TextFormField(
+            onTap: () {
+              if (title == 'Data urodzenia') {
+                FocusScope.of(context).requestFocus(new FocusNode());
+                _presentDatePicker();
+              }
+            },
             controller: controller,
             obscureText: title == 'Hasło',
             style: const TextStyle(
@@ -217,6 +256,7 @@ class _RegisterPageState extends State<RegisterPage> {
               fontFamily: "Croissant One",
             ),
             decoration: InputDecoration(
+                border: const UnderlineInputBorder(borderSide: BorderSide.none),
                 hintText: title,
                 hintStyle: const TextStyle(
                     fontFamily: 'Asap', fontStyle: FontStyle.italic),
@@ -234,12 +274,84 @@ class _RegisterPageState extends State<RegisterPage> {
               }
               return null;
             },
+          ),
+        ),
+      ],
+    );
+  }
 
+  Widget DropDownList(
+      BuildContext context, String title, TextEditingController controller) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Container(
+          width: screenWidth * 0.65,
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Text(
+            title,
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Architects Daughter',
+                fontSize: screenWidth * 0.06),
+          ),
+        ),
+        Container(
+          width: screenWidth * 0.75,
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+          margin: const EdgeInsets.only(bottom: 10, left: 0),
+          decoration: const BoxDecoration(
+            color: COLOR_INPUT_FIELDS,
+            borderRadius: BorderRadius.all(BORDER_RADIUS_INPUTS),
+          ),
+          child: DropdownButtonFormField<String>(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+            dropdownColor: COLOR_BACKGROUND_DARKER,
+            hint: const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Uczelnia',
+                style: TextStyle(
+                    fontFamily: 'Asap',
+                    fontStyle: FontStyle.italic,
+                    fontSize: 16),
+              ),
+            ),
+            decoration: const InputDecoration(
+                border: UnderlineInputBorder(borderSide: BorderSide.none),
+                hintStyle: TextStyle(fontFamily: 'Asap', fontStyle: FontStyle.italic),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent))),
+            style: const TextStyle(
+              color: Colors.white,
+              fontFamily: "Croissant One",
+            ),
+            value: _uczelniaController.text.isEmpty
+                ? null
+                : _uczelniaController.text,
+            items: Uczelnie.map((String uczelnia) {
+              return DropdownMenuItem<String>(
+                value: uczelnia,
+                child: Text(uczelnia),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                _uczelniaController.text = newValue!;
+                fieldErrors[title] = newValue.isNotEmpty;
+              });
+            },
+            validator: (value) {
+              if (fieldErrors[title] == false) {
+                autovalidateMode = AutovalidateMode.always;
+                return "Proszę uzupełnić pole";
+              }
+              return null;
+            },
           ),
         ),
       ],
     );
   }
 }
-
-
