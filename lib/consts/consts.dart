a8d2fb833ce9da9578d7a3a9ddeeb0c75db05778
Ola <245818@edu.p.lodz.pl>
2024-05-28 12:24:25 +0200

    Changed color of Input Fields

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 
 const Color COLOR_BACKGROUND_DARKER = Color.fromRGBO(49, 62, 80, 1.0);
 const Color COLOR_BACKGROUND = Color.fromRGBO(71, 82, 98, 1.0);
-const Color COLOR_INPUT_FIELDS = Color.fromRGBO(184, 225, 255, 0.5);
+const Color COLOR_INPUT_FIELDS = Color.fromRGBO(130, 153, 176, 1.0);
+const Color COLOR_RADIO_BUTTON_CHECKED = Color.fromRGBO(202, 202, 202, 1.0);
 
 const Radius BORDER_RADIUS = Radius.circular(25);
 const Radius BORDER_RADIUS_INPUTS = Radius.circular(13);
